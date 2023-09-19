4ac5884379829e0168e4d226aa4719bd5ef7c7dd
Ajay93455946 <144216210+Ajay93455946@users.noreply.github.com>
2023-09-19 15:19:54 +0530

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,16 @@
+
+class student():
+  def __init__(self,name,roll,cgpa):
+       self.name=name
+       self.roll=roll
+       self.cgpa=cgpa
+
+def sortcgpa(sortlist):
+  sortedstu=sorted(sortlist, key=lambda x: x.cgpa, reverse=True)
+  return sortedstu
+
+stuobj=[student("Abinash","22CS2101",7.9),student("Ajay","22CS2102",6.5),student("Akash","22CS2103",7.3),student("Arivu","22CS2104",6.8)]
+sortedstu=sortcgpa(stuobj)
+
+for stu in sortedstu:
+  print(f"Name: {stu.name}   Roll.no: {stu.roll}    CGPA: {stu.cgpa}")
